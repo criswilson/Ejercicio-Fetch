0980c91b5f2e83119a506b146f1da970ca6e1e27
cristian <cristian.wilson3@gmail.com>
2021-06-13 02:57:02 -0300

    scripts modificado con try/catch

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 async function fetchData() {
-  const inputValue = document.getElementById("pokemon").value.toLowerCase();
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
-  const json = await data.json();
+  try {
+    const inputValue = document.getElementById("pokemon").value.toLowerCase();
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`);
+    const json = await data.json();
 
-  document.getElementById("name").innerHTML = json.name;
-  document.getElementById("imagen").src = json.sprites.front_default;
-  document.getElementById("pokemon").value = "";
+    document.getElementById("name").innerHTML = json.name;
+    document.getElementById("imagen").src = json.sprites.front_default;
+    document.getElementById("pokemon").value = "";
+  } catch (error) {
+    console.log("Error");
+    document.getElementById("name").innerHTML = "Pokemon inválido";
+    document.getElementById("imagen").src = " ";
+  }
 }
 
 function enter(e) {
